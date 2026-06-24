e64732a42ef7076051e567e994fbe17450c45b65
iss-webdev <192451660+iss-webdev.noreply.github.com>
2026-06-24 10:50:04 +0530

    feat: Tailwind styling, two panel layout

---
+++ app/api/review/route.ts
@@ -0,0 +1,32 @@
+import { NextResponse, NextRequest } from "next/server";
+import { GoogleGenAI } from "@google/genai";
+
+export async function POST(request: NextRequest) {
+  try {
+    const body = await request.json();
+
+    if (!body.code || !body.lang) {
+      return NextResponse.json(
+        { error: "code and languge are required" },
+        { status: 400 },
+      );
+    }
+
+    const client = new GoogleGenAI({ apiKey: process.env.GEMINI_KEY });
+    const interaction = await client.interactions.create({
+      model: "gemini-3.5-flash",
+      input: `review this ${body.lang} code and give feedback: ${body.code}`,
+    });
+
+    return NextResponse.json(
+      { Message: interaction.output_text, data: body },
+      { status: 201 },
+    );
+  } catch (error) {
+    console.log(error)
+    return NextResponse.json(
+      { error: "failed to process request" },
+      { status: 500 },
+    );
+  }
+}
---
+++ app/page.tsx
@@ -0,0 +1,74 @@
+"use client";
+
+import React, { useState } from "react";
+import Editor from "@monaco-editor/react";
+
+const Page = () => {
+  const [code, setCode] = useState("");
+  const [lang, setLang] = useState("typescript");
+  const [review, setReview] = useState("");
+  const [preload, setPreload] = useState(false);
+
+  async function handleReview() {
+    console.log(code, lang);
+    setPreload(true);
+    const data = await fetch("/api/review", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ code, lang }),
+    }).then((res) => res.json());
+    console.log(data);
+    setReview(data.Message);
+    setPreload(false);
+  }
+
+  return (
+    <div className="flex h-screen">
+      <div className="w-1/2 p-4">
+        <div className="flex items-center justify-between">
+          <h1>Code</h1>
+          <div className="flex items-center gap-2">
+            <h1>Language:</h1>{" "}
+            <select className="text-black bg-white p-1 rounded cursor-pointer" value={lang} onChange={(e) => setLang(e.target.value)}>
+              <option value="javascript">javascript</option>
+              <option value="react">react</option>
+              <option value="go">go</option>
+              <option value="typescript">typescript</option>
+              <option value="python">python</option>
+              <option value="c">c</option>
+              <option value="cpp">cpp</option>
+            </select>
+          </div>
+        </div>
+        <Editor
+          height="600px"
+          theme="vs-dark"
+          language={lang}
+          value={code}
+          onChange={(value) => setCode(value ?? " ")}
+        />
+        <div className="flex justify-end">
+          <button
+            className="p-3 cursor-pointer rounded bg-white text-black"
+            onClick={handleReview}
+          >
+            Submit
+          </button>
+        </div>
+      </div>
+
+      <div className="w-1/2 p-4 flex flex-col h-full">
+        <h2>Code Review</h2>
+        {preload ? (
+          <h4 className="flex-1 flex items-center justify-center ">
+            Reviewing...
+          </h4>
+        ) : (
+          <p>{review}</p>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Page;
