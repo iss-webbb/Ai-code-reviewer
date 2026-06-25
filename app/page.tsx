074215b0574aa73ac3d3ac5e943d64ab217a6495
iss-webdev <192451660+iss-webdev.noreply.github.com>
2026-06-25 15:04:44 +0530

    added example code

--- app/page.tsx
+++ app/page.tsx
@@ -2,16 +2,15 @@
 
 import React, { useState } from "react";
 import Editor from "@monaco-editor/react";
-import ReactMarkDown from 'react-markdown';
+import ReactMarkDown from "react-markdown";
 import remarkGfm from "remark-gfm";
-
 
 const Page = () => {
   const [code, setCode] = useState("");
   const [lang, setLang] = useState("typescript");
   const [review, setReview] = useState("");
   const [preload, setPreload] = useState(false);
-  const [err, setErr] = useState("")
+  const [err, setErr] = useState("");
 
   async function handleReview() {
     setPreload(true);
@@ -21,18 +20,35 @@ const Page = () => {
       body: JSON.stringify({ code, lang }),
     }).then((res) => res.json());
     setReview(data.Message);
-    setErr(data.error)
+    setErr(data.error);
     setPreload(false);
   }
+
+  function exampleCode() {
+    setCode(`const greet = (name: string): string => \`Hello, \${name}!\`;
+    console.log(greet("joe"));`);
+  }
+
+  const display = code === "" ? " " : review;
 
   return (
     <div className="flex h-screen">
       <div className="w-1/2 p-4">
         <div className="flex items-center justify-between">
           <h1>Code</h1>
+            <button
+              className="bg-white text-black p-1 rounded cursor-pointer"
+              onClick={exampleCode}
+            >
+              Example Code
+            </button>
           <div className="flex items-center gap-2">
             <h1>Language:</h1>{" "}
-            <select className="text-black bg-white p-1 rounded cursor-pointer" value={lang} onChange={(e) => setLang(e.target.value)}>
+            <select
+              className="text-black bg-white p-1 rounded cursor-pointer"
+              value={lang}
+              onChange={(e) => setLang(e.target.value)}
+            >
               <option value="javascript">javascript</option>
               <option value="react">react</option>
               <option value="go">go</option>
@@ -67,11 +83,15 @@ const Page = () => {
             Reviewing...
           </h4>
         ) : (
-            <div><article className="pb-5 pt-4 prose prose-sm max-w-none"><ReactMarkDown remarkPlugins={[remarkGfm]}>{review}</ReactMarkDown></article>
-          <p className="flex-1 flex items-center justify-center">{err}</p></div>
-          
+          <div>
+            <article className="pb-5 pt-4 prose prose-sm max-w-none">
+              <ReactMarkDown remarkPlugins={[remarkGfm]}>
+                {display}
+              </ReactMarkDown>
+            </article>
+            <p className="flex-1 flex items-center justify-center">{err}</p>
+          </div>
         )}
-
       </div>
     </div>
   );
